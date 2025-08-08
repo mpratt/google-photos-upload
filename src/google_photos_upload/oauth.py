b9cf46634867b759c54324cb2ea94f7a31a78228
Michael Pratt <commits@michael-pratt.com>
2025-08-08 11:08:44 -0500

    fix: Updated the app scopes in order to be compliant to the new google API changes - Fixed some formatting

--- src/google_photos_upload/oauth.py
+++ src/google_photos_upload/oauth.py
@@ -4,23 +4,27 @@ from datetime import datetime
 from google_auth_oauthlib.flow import InstalledAppFlow
 from requests_oauthlib import OAuth2Session
 
+
 class OAuth:
     def __init__(self, credentials) -> None:
         self.session = None
-        file = open(credentials, 'r')
+        file = open(credentials, "r")
         self.credentials_file = credentials
         self.credentials_data = json.load(file)
-        self.token_file = "{}/{}.token".format(os.path.dirname(self.credentials_file), self.credentials_data['installed']['client_id'])
+        self.token_file = "{}/{}.token".format(
+            os.path.dirname(self.credentials_file),
+            self.credentials_data["installed"]["client_id"],
+        )
 
     def load_token(self):
         try:
-            file = open(self.token_file, 'r')
+            file = open(self.token_file, "r")
             return json.load(file)
         except (json.JSONDecodeError, IOError):
             return None
 
     def save_token(self, token):
-        file = open(self.token_file, 'w')
+        file = open(self.token_file, "w")
         file.write(json.dumps(token))
         file.close()
 
@@ -28,15 +32,17 @@ class OAuth:
         token = self.load_token()
         if token:
             now = datetime.now()
-            expiration = token['expires_at']
+            expiration = token["expires_at"]
             if datetime.timestamp(now) <= expiration:
                 self.session = OAuth2Session(
-                    self.credentials_data['installed']['client_id'],
+                    self.credentials_data["installed"]["client_id"],
                     token=token,
-                    auto_refresh_url=self.credentials_data['installed']['token_uri'],
-                    auto_refresh_kwargs= {
-                        'client_id': self.credentials_data['installed']['client_id'],
-                        'client_secret': self.credentials_data['installed']['client_secret']
+                    auto_refresh_url=self.credentials_data["installed"]["token_uri"],
+                    auto_refresh_kwargs={
+                        "client_id": self.credentials_data["installed"]["client_id"],
+                        "client_secret": self.credentials_data["installed"][
+                            "client_secret"
+                        ],
                     },
                     token_updater=self.save_token,
                 )
@@ -44,14 +50,13 @@ class OAuth:
                 return None
 
         flow = InstalledAppFlow.from_client_secrets_file(
-            self.credentials_file, scopes=[
-                'https://www.googleapis.com/auth/photoslibrary'
-            ]
+            self.credentials_file,
+            scopes=[
+                "https://www.googleapis.com/auth/photoslibrary.appendonly",
+            ],
         )
 
-        flow.run_local_server(
-            open_browser=False, bind_addr="0.0.0.0", port=8089
-        )
+        flow.run_local_server(open_browser=False, bind_addr="0.0.0.0", port=8089)
 
         self.session = flow.authorized_session()
         oauth2_token = {
@@ -66,17 +71,19 @@ class OAuth:
         return None
 
     def make_request(self, method, path, data):
-        return self.session.request(method, path, data = json.dumps(data))
+        return self.session.request(method, path, data=json.dumps(data))
 
     def make_upload(self, method, path, data, timeout):
         headers = {
-            'Content-type': 'application/octet-stream',
-            'X-Goog-Upload-Protocol': 'raw',
-            'X-Goog-File-Name': os.path.basename(data)
+            "Content-type": "application/octet-stream",
+            "X-Goog-Upload-Protocol": "raw",
+            "X-Goog-File-Name": os.path.basename(data),
         }
 
-        file = open(data, 'rb')
+        file = open(data, "rb")
         if timeout is None:
             return self.session.request(method, path, data=file.read(), headers=headers)
         else:
-            return self.session.request(method, path, data=file.read(), headers=headers, timeout=timeout)
+            return self.session.request(
+                method, path, data=file.read(), headers=headers, timeout=timeout
+            )
